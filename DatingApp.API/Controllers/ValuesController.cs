dedb6b5b49ab37d88dc4d419ed0de74c798864b0
Mohammad Ansari <ansari.mohammad@outlook.com>
2020-05-25 14:09:24 +0430

    End  of registration and login

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -18,6 +18,7 @@ namespace DatingApp.API.Controllers
             this._context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
